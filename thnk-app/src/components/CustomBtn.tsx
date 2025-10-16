b333f519318ba168ed842d0d751155efa4185642
mwape-k <82158230+mwape-k@users.noreply.github.com>
2025-10-16 17:37:22 +0200

    CORE(build): building UI components

--- thnk-app/src/components/CustomBtn.tsx
+++ thnk-app/src/components/CustomBtn.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-function CustomBtn() {
+interface buttonProps {
+  text?: string; 
+  onClick?: () => void;
+  className?: string;
+  type?: 'button' | 'submit' 
+  isLoading?: boolean;
+  size?: "full" | "large" | "medium" | "small" | "auto";
+}
+
+const CustomBtn: React.FC<buttonProps> = ({text = 'Button text', onClick, className = "", type= "button", isLoading = false, size = "full", }) => {
   return (
-    <button className="btn btn-primary">Primary</button>
+    <button type={type} className={`custom-button custom-button--${size} ${className} ${
+        isLoading ? "custom-button--isLoading" : ""
+      }`}
+      onClick={onClick}><span className="custom-button__text">{text}</span></button>
   )
 }
 
