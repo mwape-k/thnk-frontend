99ae45b82011ef1558bff538d25ca03c0c6f6857
mwape-k <82158230+mwape-k@users.noreply.github.com>
2025-11-04 20:47:02 +0200

    FIX: navbar misconfiguration on deployed frontend

--- thnk-app/src/App.tsx
+++ thnk-app/src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import UserAuthPage from "./pages/UserAuthPage";
 import UserHistoryPage from "./pages/UserHistoryPage";
@@ -10,10 +15,19 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+
+        {/*for vercel rewrites*/}
+        <Route path="/index.html" element={<Navigate to="/" replace />} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        {/*for vercel rewrites*/}
+
         <Route path="/User-auth" element={<UserAuthPage />} />
         <Route path="/User-history" element={<UserHistoryPage />} />
         <Route path="/Search-results" element={<ResultsPage />} />
         <Route path="/Deeper-analysis" element={<DeeperSeekPage />} />
+
+        {/*catch all for vercel*/}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
